8a07831bbfdb8130b919c09afadc1ee0b5ddeac8
Akshaj Bhandari <84961610+akshajbhandari28@users.noreply.github.com>
2026-08-29 16:47:55 +0530

    Update __init__.py

---
+++ charge-and-chew/charge-and-chew/backend/__init__.py
@@ -0,0 +1,8 @@
+from backend.trip_planner import plan_trip, VEHICLE_PROFILES
+from backend.range_engine import VehicleProfile
+
+__all__ = [
+    "plan_trip",
+    "VEHICLE_PROFILES",
+    "VehicleProfile",
+]
